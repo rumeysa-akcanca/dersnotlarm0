bcd31ea72f10c4e019e751a4ffcb68a56fcfaa7b
rumeysa-akcanca <94036242+rumeysa-akcanca@users.noreply.github.com>
2021-12-08 08:34:18 +0300

    ik
    çözümlendi

---
+++ İK project/İK project/Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İK_project
+{
+    class Address
+    {
+
+        public string city { get; set; }
+        public string country { get; set; }
+        public int HouseNumber { get; set; }
+        public string Street{ get; set; }
+        public  int ZipCode { get; set; }
+        public int ID { get;  }
+        public Address(int ıd)
+        {
+            this.ID = ıd;
+        }
+       
+        public Address(string city, string country, int houseNumber, string street, int zipCode)
+        {
+            this.city = city;
+            this.country = country;
+            HouseNumber = houseNumber;
+            Street = street;
+            ZipCode = zipCode;
+        }
+
+
+    }
+}
---
+++ İK project/İK project/Department.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İK_project
+{
+     class Department
+    {
+
+        public List<Employee> employees { set; get; }
+        public Manager HeadOF { get; set; }
+        public int ID{ get;  }
+        public String  Name { get; set; }
+        int idcounter;
+        public Department()
+        {
+
+        }
+        public Department(Employee employee, Manager headOF, int ID, string name)
+        {
+            List<Employee> employees = new List<Employee>();
+            HeadOF = headOF;
+            Name = name;
+            ID += idcounter;
+        }
+
+
+    }
+}
